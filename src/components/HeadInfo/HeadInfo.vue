c730386f3b6087129a8c5afee85ba2306ee7bbce
Anna Zubareva <annazubareva14@gmail.com>
2022-01-31 16:32:59 +0300

    EPMLSTRJV-11 add head info

---
+++ src/components/HeadInfo/HeadInfo.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="wrapper">
+    <div class="info">
+      <div class="name">{{ headInfo.name }}</div>
+      <div class="date__wrapper">
+        date: <span class="date">{{ headInfo.date }} </span>
+      </div>
+    </div>
+    <div class="price__wrapper">
+      <span class="price">{{ headInfo.price }} </span> $
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BtdHeadInfo',
+
+  props: {
+    headInfo: {
+      type: Object,
+      default: () => ({
+        name: '',
+        date: 0,
+        price: 0
+      })
+    }
+  }
+};
+</script>
+
+//
+<style lang="scss" scoped>
+@import '../../scss/variables.scss';
+.wrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #fff;
+  width: 100%;
+  padding: 20px 10px;
+  box-shadow: $shadow;
+}
+
+.name {
+  font-size: $font-size-subtitle;
+  color: $font-color-title;
+}
+
+.date__wrapper {
+  color: $font-color-text;
+}
+
+.price__wrapper {
+  font-size: 28px;
+  color: $font-color-title;
+}
+</style>
---
+++ src/components/HeadInfo/__tests__/HeadInfo.spec.js
@@ -0,0 +1,39 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import BtdHeadInfo from '../HeadInfo.vue';
+
+describe('HeadInfo.vue', () => {
+  let wrapper;
+  const testName = 'name';
+  const testDate = '00.00.0000';
+  const testPrice = '0';
+  const name = () => wrapper.find('.name');
+  const date = () => wrapper.find('.date');
+  const price = () => wrapper.find('.price');
+  const localVue = createLocalVue();
+  const renderWrapper = () => {
+    wrapper = shallowMount(BtdHeadInfo, {
+      localVue,
+      propsData: {
+        headInfo: {
+          name: testName,
+          date: testDate,
+          price: testPrice
+        }
+      }
+    });
+  };
+
+  beforeEach(() => {
+    renderWrapper();
+  });
+
+  it('should match snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should insert correct data', () => {
+    expect(name().text()).toBe(testName);
+    expect(date().text()).toBe(testDate);
+    expect(price().text()).toBe(testPrice);
+  });
+});
