c39af969f8943a58862bbd97a0198b777bd84992
Alex Isakov <Alexey_Isakov@epam.com>
2022-02-02 22:11:47 +0300

    *Added **Main Icon** and **Cart Icon** svg icons into _"/assets/icons"_ folder
    *Added **Welcome Title** component which is filled with props
    
    *Added **CustomIcon ** component that takes name of an icon as props. Icons' size is _adjusted by **width** props_
    (e.g icon="shopping_cart") )
    
    *Wrote basic snapshot tests
    
    @Konstantin_Zharich @dinar_nizmutdinov please review and merge

---
+++ src/components/BaseComponents/WelcomeTitle/WelcomeTitle.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="welcome-title-banner">
+    <h1 class="welcome-title">
+      {{ title }}
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WelcomeTitle',
+
+  props: {
+    title: [String, Number]
+  }
+};
+</script>
+
+<style scoped>
+.welcome-title {
+  color: #0000ff;
+  font-size: 40px;
+  font-weight: lighter;
+}
+.welcome-title-banner {
+  display: inline-block;
+  margin: 5px;
+}
+</style>
