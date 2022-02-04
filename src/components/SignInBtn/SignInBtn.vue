6f270ccb462f1e392c6edcf5cf1986d0c2c44844
yuliya-karimova <y.o.karimova@gmail.com>
2022-02-04 14:04:48 +0300

    feat: add SignInBtn.vue

---
+++ src/components/SignInBtn/SignInBtn.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="sign-in__button">
+    <BaseButtun v-if="isSignIn" v-on:click="onClickShowPopup" />
+    <BaseButtonRouter v-else :path="profilePath"> {{ profileBtnText }} </BaseButtonRouter>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignInBtn',
+
+  components: {
+    BaseButtun,
+    BaseButtonRouter
+  },
+
+  props: {
+    isSignIn: {
+      type: Boolean,
+      required: true,
+      default: false,
+    }
+  },
+
+  methods: {
+    onClickShowPopup: function () {
+      this.$emit('onClickShowPopup');
+    }
+  },
+
+  data() {
+    return {
+      profilePath: '/profile',
+      profileBtnText: 'Profile',
+    }
+  }
+}
+</script>
+
+<style scoped>
+.sign-in__button {
+  padding: 5px;
+}
+</style>
