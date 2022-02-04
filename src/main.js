ba0f23eb87d52573a7141d70dd3fe7625bf288dc
Ekaterina Sotnik <ekaterina_sotnik@epam.com>
2022-02-04 13:37:10 +0300

    renamed 'BaseComponents' folder to 'base components' and added index.js there, fixed imports to main.js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import BaseButtonRouter from './BaseComponents/BaseButtonRouter';
-import BaseButton from './BaseComponents/BaseButton';
-import BaseButtonLink from './BaseComponents/BaseButtonLink';
+import * as BaseComponents from './base components';
 import '@/assets/styles/index.scss';
 
 Vue.config.productionTip = false;
@@ -12,8 +10,6 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
-  BaseButtonRouter,
-  BaseButton,
-  BaseButtonLink,
+  BaseComponents,
   render: (h) => h(App)
 }).$mount('#app');
