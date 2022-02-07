67aae6e2f69a5f48ed02f322d326dfcfcd9259a1
Ekaterina Sotnik <ekaterina_sotnik@epam.com>
2022-02-07 13:18:41 +0300

    delete duplicate folder ('base components')

--- src/base components/index.js
+++
@@ -1,5 +0,0 @@
-import BaseButtonRouter from './BaseButtonRouter/BaseButtonRouter.vue';
-import BaseButton from './BaseButton/BaseButton.vue';
-import BaseButtonLink from './BaseButtonLink/BaseButtonLink.vue';
-
-export { BaseButton, BaseButtonLink, BaseButtonRouter };
