a88e6766cb39318a4d7183e302fed29121665aba
yuliya-karimova <y.o.karimova@gmail.com>
2022-02-09 18:29:23 +0300

    fix: restore ComponentsPage

--- src/pages/ComponentsPage/ComponentsPage.vue
+++ src/pages/ComponentsPage/ComponentsPage.vue
@@ -1,54 +1,41 @@
 <template>
   <div>
-    <SignInBtn
-      :isSignIn="isSignIn"
-      @on-click-show-sign-in-popup="onClickShowSignInPopup"
-    />
-    <SignInPopup
-      :isPopupOpened="isPopupOpened"
-      @sign-un="signIn"
-      @sign-up="signUp"
-      @on-click-close-popup="onClickCloseSignInPopup"
-    />
+    <div>
+      <div class="title">Buttons</div>
+      <div>
+        <div class="sub-title">Flat</div>
+        <md-button>Default</md-button>
+        <md-button class="md-primary">Primary</md-button>
+        <md-button class="md-accent">Accent</md-button>
+        <md-button disabled>Disabled</md-button>
+      </div>
+
+      <div>
+        <div class="sub-title">Raised</div>
+        <md-button class="md-raised">Default</md-button>
+        <md-button class="md-raised md-primary">Primary</md-button>
+        <md-button class="md-raised md-accent">Accent</md-button>
+        <md-button class="md-raised" disabled>Disabled</md-button>
+      </div>
+    </div>
+    <div>
+      <div class="title">Tabs</div>
+      <md-tabs class="md-transparent" md-alignment="fixed">
+        <md-tab id="tab-1" md-label="Sign In"></md-tab>
+        <md-tab id="tab-2" md-label="Sign Up"></md-tab>
+      </md-tabs>
+    </div>
   </div>
 </template>
 
 <script>
 import Vue from 'vue';
 import VueMaterial from 'vue-material';
-import { SignInBtn, SignInPopup } from '@/components/SignIn/';
 
 Vue.use(VueMaterial);
 
 export default {
-  name: 'ComponentsPage',
-
-  components: { SignInBtn, SignInPopup },
-
-  data() {
-    return {
-      isPopupOpened: false,
-      isSignIn: false
-    };
-  },
-
-  methods: {
-    onClickShowSignInPopup: function () {
-      this.isPopupOpened = true;
-    },
-
-    onClickCloseSignInPopup: function () {
-      this.isPopupOpened = false;
-    },
-
-    signIn() {
-      this.isSignIn = true;
-    },
-
-    signUp() {
-      this.isSignIn = true;
-    }
-  }
+  name: 'ComponentsPage'
 };
 </script>
 
