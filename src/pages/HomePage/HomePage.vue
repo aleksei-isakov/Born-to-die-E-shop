5a1a40cc382a3cd713ed9e9f77bd8730c1636532
Ekaterina Sotnik <ekaterina_sotnik@epam.com>
2022-02-10 12:27:32 +0300

    removed Header and Footer from the HomePage and added into the App.vue

--- src/pages/HomePage/HomePage.vue
+++ src/pages/HomePage/HomePage.vue
@@ -1,23 +1,17 @@
 <template>
   <div>
-    <Header />
     <ProductsList />
-    <Footer />
   </div>
 </template>
 
 <script>
 import ProductsList from '@/components/ProductsList/ProductsList.vue';
-import Header from '../../components/Header/Header.vue';
-import Footer from '../../components/Footer/Footer.vue';
 
 export default {
   name: 'HomePage',
 
   components: {
-    ProductsList,
-    Header,
-    Footer
+    ProductsList
   }
 };
 </script>
