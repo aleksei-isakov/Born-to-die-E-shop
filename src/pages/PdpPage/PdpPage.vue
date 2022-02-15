c6a66fbbb69dbfb9bbcdc750e66f596db2c21a5e
Maksim Sychev <maksim_sychev@epam.com>
2022-02-15 12:08:25 +0300

    fix EPMLSTRJV-12

--- src/pages/PdpPage/PdpPage.vue
+++ src/pages/PdpPage/PdpPage.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="pdp__wrapper">
-    <HeadInfo :name="name" />
-    <Slider class="slider" />
-    <ProductDetails
+    <head-info :name="name" />
+    <slider class="slider" />
+    <product-details
       :productDescription="productDescription"
       class="productDetails"
     />
@@ -13,6 +13,7 @@
 import Slider from '@/components/Slider/Slider';
 import ProductDetails from '@/components/ProductDetails/ProductDetails';
 import HeadInfo from '@/components/HeadInfo/HeadInfo';
+import description from './PdpPageMock.json';
 
 export default {
   name: 'PdpPage',
@@ -20,8 +21,7 @@ export default {
   components: { ProductDetails, Slider, HeadInfo },
 
   data: () => ({
-    productDescription:
-      'AirPods Pro — уникальные наушники с технологией активного шумоподавления, которые непрерывно оптимизируют звучание с учётом формы вашего уха и положения вкладышей. Они отлично заглушают внешние шумы, поэтому вы можете полностью погрузиться в музыку. Наружный микрофон улавливает внешние шумы. Эти шумы компенсируются обратной звуковой волной той же амплитуды — прежде чем вы их услышите. Внутренний микрофон анализирует звук внутри уха и таким же образом подавляет оставшийся шум обратной звуковой волной. В комплекте есть три пары мягких силиконовых вкладышей, которые легко фиксируются на наушнике. Специальный тест поможет выбрать размер вкладышей для оптимальной посадки и лучшего звука. Внутри каждого вкладыша есть особая коническая полость, которая помогает ему лучше адаптироваться к геометрии уха и надёжнее держать наушник. А отверстия во вкладышах позволяют выравнять давление, поэтому наушники будут для вас совершенно незаметны.',
+    productDescription: description,
 
     name: 'Airpods'
   })
