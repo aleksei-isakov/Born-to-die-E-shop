a0bf14c870b8d02fd59170314efb1fd8ad531301
Anna Zubareva <annazubareva14@gmail.com>
2022-02-17 19:16:04 +0300

    EPMLSTRJV-57 change to axios

--- src/api/setup.js
+++ src/api/setup.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
+import { BASE_URL } from '@/constants.js';
 
 const HEADERS = {
   'Content-Type': 'application/json'
 };
-const host = process.env.VUE_APP_API_HOST ? process.env.VUE_APP_API_HOST : '';
-const requestSettings = {
-  baseURL: `${host}/api/v1`,
+const instance = axios.create({
+  baseURL: `${BASE_URL}/api/v1`,
   headers: HEADERS
-};
+});
 
-export const http = axios.create(requestSettings);
+export default instance;
--- src/store/modules/products/actions.js
+++ src/store/modules/products/actions.js
@@ -1,17 +1,15 @@
+import axios from '@/api/setup.js';
 import mutationTypes from './mutationTypes';
 
 const actions = {
   async getProductsList({ commit }) {
-    fetch('http://localhost:3000/664/products')
-      .then((result) => {
-        return result.json();
-      })
-      .then((data) => {
-        commit(mutationTypes.SET_PRODUCTS_SUCCESS, data);
-      })
-      .catch((error) => {
-        commit(mutationTypes.SET_PRODUCTS_FAIL, error.code);
-      });
+    try {
+      const { data } = await axios.get(`/products`);
+
+      commit(mutationTypes.SET_PRODUCTS_SUCCESS, data);
+    } catch (error) {
+      commit(mutationTypes.SET_PRODUCTS_FAIL, error.code);
+    }
   }
 };
 
