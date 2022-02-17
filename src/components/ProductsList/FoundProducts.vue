1f0ec081f9d6f0d569fd3ff502b2b47d1d53a6a0
Flyingcanoee <nastya_tkachenko@list.ru>
2022-02-17 16:22:12 +0300

    Fix FoundProducts component name

---
+++ src/components/ProductsList/FoundProducts.vue
@@ -0,0 +1,27 @@
+<template>
+  <span class="found-products">Found: {{ itemsPerPage }}</span>
+</template>
+
+<script>
+export default {
+  name: 'FoundProducts',
+
+  props: {
+    itemsPerPage: {
+      type: Number,
+      required: true,
+      default: 1
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/scss/variables.scss';
+
+.found-products {
+  font-weight: bold;
+  color: $primary;
+  margin: 20px 0;
+}
+</style>
