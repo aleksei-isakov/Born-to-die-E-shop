77f55c3607e44fab4100a69d85d3a6f55da99911
yuliya-karimova <y.o.karimova@gmail.com>
2022-03-04 16:57:53 +0300

    improve: improve tests for HeadInfo, add tests for computed properties

--- src/components/HeadInfo/__tests__/HeadInfo.spec.js
+++ src/components/HeadInfo/__tests__/HeadInfo.spec.js
@@ -1,20 +1,15 @@
 import { createLocalVue, shallowMount } from '@vue/test-utils';
 import BtdHeadInfo from '../HeadInfo.vue';
-import { format } from 'date-fns';
 
 describe('HeadInfo.vue', () => {
   let wrapper;
   const testName = 'name';
   const testDate = 'January 31, 2022, 00:00:00';
-  const formatDate = format(new Date(testDate), 'DD.MM.YYYY');
   const testPrice = 130;
   const name = () => wrapper.find('.head-info__name');
   const date = () => wrapper.find('.head-info__date');
   const price = () => wrapper.find('.head-info__price');
   const localVue = createLocalVue();
-
-  jest.mock('date-fns', () => ({ format: jest.fn() }));
-
   const renderWrapper = () => {
     wrapper = shallowMount(BtdHeadInfo, {
       localVue,
@@ -38,9 +33,17 @@ describe('HeadInfo.vue', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should insert correct data', () => {
+  it('computed property "getDate" should return date in the format "DD.MM.YYYY"', () => {
+    expect(wrapper.vm.getDate).toBe('31.01.2022');
+  });
+
+  it('computed property "getPrice" should return price in the format "130.0"', () => {
+    expect(wrapper.vm.getPrice).toBe('130.0');
+  });
+
+  it('should display: name; date in the format "DD.MM.YYYY"; price in the format "130.0 $" to the user', () => {
     expect(name().text()).toBe(testName);
-    expect(date().text()).toBe(`date: ${formatDate}`);
-    expect(price().text()).toBe(`${testPrice.toFixed(1)} $`);
+    expect(date().text()).toBe(`date: ${wrapper.vm.getDate}`);
+    expect(price().text()).toBe(`${wrapper.vm.getPrice} $`);
   });
 });
