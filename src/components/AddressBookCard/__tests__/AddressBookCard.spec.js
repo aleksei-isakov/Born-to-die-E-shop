5534254ba5a42e95b898239c096f9c55fd39e6d7
Flyingcanoee <nastya_tkachenko@list.ru>
2022-04-11 22:01:11 +0300

    fix issues from code review

--- src/components/AddressBookCard/__tests__/AddressBookCard.spec.js
+++ src/components/AddressBookCard/__tests__/AddressBookCard.spec.js
@@ -2,7 +2,7 @@ import { mount } from '@vue/test-utils';
 import AddressBookCard from '../AddressBookCard.vue';
 
 describe('AddressBookCard', () => {
-  let wrapper = mount(AddressBookCard, {});
+  const wrapper = mount(AddressBookCard);
 
   it('should render edit button', () => {
     expect(wrapper.find('.address-card__button').exists()).toBe(true);
