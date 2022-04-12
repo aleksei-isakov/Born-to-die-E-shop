2dcabd08d1c010a2428c8960affd7e7d3782fa4c
Flyingcanoee <nastya_tkachenko@list.ru>
2022-04-12 12:31:12 +0300

    Add mock data into tests

--- src/components/AddressBookCard/__tests__/AddressBookCard.spec.js
+++ src/components/AddressBookCard/__tests__/AddressBookCard.spec.js
@@ -1,8 +1,16 @@
 import { mount } from '@vue/test-utils';
 import AddressBookCard from '../AddressBookCard.vue';
+import mockInfo from '../addressBookCardMock.json';
 
 describe('AddressBookCard', () => {
-  const wrapper = mount(AddressBookCard);
+  const wrapper = mount(AddressBookCard, {
+    propsData: {
+      name: mockInfo[0].firstName,
+      address: mockInfo[0].city,
+      phone: mockInfo[0].phone,
+      zipcode: mockInfo[0].zipCode
+    }
+  });
 
   it('should render edit button', () => {
     expect(wrapper.find('.address-card__button').exists()).toBe(true);
