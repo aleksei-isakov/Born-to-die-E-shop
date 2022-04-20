3ce07e8c8639ab4c58eb65b26a064354da142de5
Ekaterina Sotnik <ekaterina_sotnik@epam.com>
2022-04-21 00:31:53 +0300

    added ShoppingCartList

--- src/pages/CheckoutPage/CheckoutPage.vue
+++ src/pages/CheckoutPage/CheckoutPage.vue
@@ -1,11 +1,29 @@
 <template>
-  <div class="checkout-page__wrapper">
+  <div class="checkout-page__wrapper page-container">
     <router-view />
+    <shopping-cart-list
+      class="checkout-page__product-list-container"
+      :products-in-cart="checkedProductsInCart"
+      :total-price="totalPrice"
+    />
   </div>
 </template>
 
 <script>
+import ShoppingCartList from '@/components/ShoppingCartList/ShoppingCartList';
+import { mapGetters } from 'vuex';
+
 export default {
-  name: 'CheckoutPage'
+  name: 'CheckoutPage',
+
+  components: { ShoppingCartList },
+
+  computed: {
+    ...mapGetters('ShoppingCartModule', [
+      'productsInCart',
+      'totalPrice',
+      'checkedProductsInCart'
+    ])
+  }
 };
 </script>
