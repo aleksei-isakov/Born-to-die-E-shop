df432cc15bd41efab58834775bae18e5ee1bd26d
Alex Isakov <Alexey_Isakov@epam.com>
2022-04-25 17:05:38 +0300

    feature: Added an icon which opens a dialog window with a choice to delete current address

--- src/components/AddressBookCard/AddressBookCard.vue
+++ src/components/AddressBookCard/AddressBookCard.vue
@@ -1,26 +1,54 @@
 <template>
   <div class="address-card">
     <div class="address-card__info">
-      <p>
-        {{ name }} <br />
-        {{ phone }}
-      </p>
-      <p>{{ address }}</p>
-      <p>{{ zipcode }}</p>
+      <div class="info-block">
+        <p>
+          {{ name }} <br />
+          {{ phone }}
+        </p>
+        <p>{{ address }}</p>
+        <p>{{ zipcode }}</p>
+      </div>
+
+      <div class="trash-icon-block" @click="onClickToggleDialog">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          role="img"
+          width="100%"
+          height="100%"
+          preserveAspectRatio="xMidYMid meet"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="red"
+            d="M9 3v1H4v2h1v13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6h1V4h-5V3H9m0 5h2v9H9V8m4 0h2v9h-2V8Z"
+          />
+        </svg>
+      </div>
     </div>
     <base-text-filled-button class="address-card__button">
       EDIT
     </base-text-filled-button>
+    <AddressDeleteDialog
+      :is-dialog-visible="isDialogVisible"
+      @onClickCloseDialog="onClickCloseDialog"
+    >
+      Are you sure you want to delete the current delivery address?
+    </AddressDeleteDialog>
   </div>
 </template>
 
 <script>
 import { BaseTextFilledButton } from '@/base_components/';
+import AddressDeleteDialog from '@/components/AddressBookCard/AddressDeleteDialog';
+import addresses from '@/components/AddressBookCard/addressBookCardMock.json';
 
 export default {
   name: 'AddressBookCard',
 
   components: {
+    AddressDeleteDialog,
     BaseTextFilledButton
   },
 
@@ -44,6 +72,20 @@ export default {
       type: String,
       default: ''
     }
+  },
+
+  data: () => ({
+    isDialogVisible: false,
+    addresses: addresses
+  }),
+
+  methods: {
+    onClickToggleDialog() {
+      this.isDialogVisible = !this.isDialogVisible;
+    },
+    onClickCloseDialog() {
+      this.isDialogVisible = false;
+    }
   }
 };
 </script>
@@ -66,6 +108,19 @@ export default {
   &__button {
     width: fit-content;
     align-self: flex-end;
+  }
+
+  &__info {
+    display: flex;
+    align-items: flex-start;
+  }
+}
+
+.trash-icon-block {
+  width: 40px;
+  height: 30px;
+  &:hover {
+    cursor: pointer;
   }
 }
 </style>
---
+++ src/components/AddressBookCard/AddressDeleteDialog.vue
@@ -0,0 +1,96 @@
+<template>
+  <div
+    v-if="isDialogVisible"
+    class="dialog-wrapper"
+    @click="onClickCloseDialog"
+  >
+    <div class="form-container" @click.stop>
+      <div class="form-container__dialog-message">
+        <p class="close-dialog-message">
+          <slot></slot>
+        </p>
+      </div>
+      <div class="form-container__button-placeholder">
+        <base-text-filled-button
+          color="custom-red"
+          @click="onClickDeleteAddress"
+        >
+          Delete
+        </base-text-filled-button>
+
+        <base-text-filled-button @click="onClickCloseDialog">
+          Cancel
+        </base-text-filled-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { BaseTextFilledButton } from '@/base_components';
+
+export default {
+  name: 'AddressDeleteDialog',
+
+  components: {
+    BaseTextFilledButton
+  },
+
+  props: {
+    isDialogVisible: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  methods: {
+    onClickCloseDialog() {
+      this.$emit('onClickCloseDialog');
+    },
+    onClickDeleteAddress() {
+      this.$emit('onClickCloseDialog');
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/scss/CustomVariables.scss';
+
+.dialog-wrapper {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: $z-index-popup;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.form-container {
+  display: flex;
+  flex-direction: column;
+  width: 400px;
+  height: 150px;
+  background-color: #e6e6e6;
+  padding: 30px;
+  &__button-placeholder {
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-between;
+  }
+  &__dialog-message {
+    width: 100%;
+    height: 100px;
+  }
+}
+
+.close-dialog-message {
+  margin-top: -10px;
+  color: $font-color-subtitle;
+  font-size: $font-size-basic;
+}
+</style>
