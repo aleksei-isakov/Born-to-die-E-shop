99d0f2a3f170913fb5d451adce346ea76898a09c
Ekaterina Sotnik <ekaterina_sotnik@epam.com>
2022-04-25 11:07:49 +0300

    minor fix

--- src/store/modules/orders/mutations.js
+++ src/store/modules/orders/mutations.js
@@ -4,9 +4,11 @@ const mutations = {
   [mutationTypes.SET_ORDERS_LOADING](state) {
     state.isLoading = true;
   },
+
   [mutationTypes.SET_ORDERS_SUCCESS](state, data) {
     state.ordersList = data;
   },
+
   [mutationTypes.SET_ORDERS_FAIL](state, status) {
     state.errorCode = status;
   }
