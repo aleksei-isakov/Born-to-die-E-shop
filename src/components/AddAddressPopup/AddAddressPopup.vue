94e061a8f9a0053c50a8f2e85cf9b201145638b2
Ekaterina Kuzina <Ekaterina_Kuzina@epam.com>
2022-04-27 13:53:50 +0300

    fix style for popup

--- src/components/AddAddressPopup/AddAddressPopup.vue
+++ src/components/AddAddressPopup/AddAddressPopup.vue
@@ -12,8 +12,7 @@
           <v-icon> close </v-icon>
         </v-btn>
       </div>
-
-      <v-form v-model="valid" @submit.prevent="onSubmit">
+      <v-form v-model="valid" class="form" @submit.prevent="onSubmit">
         <div class="form_wrapper">
           <v-row>
             <v-col class="form-title" md="2">
@@ -431,6 +430,7 @@ export default {
   z-index: $z-index-popup;
   display: flex;
   background-color: rgba(0, 0, 0, 0.5);
+  overflow-y: auto;
 }
 
 .popup-container {
@@ -455,6 +455,7 @@ export default {
 .form_wrapper {
   width: 100%;
   padding: 20px 20px;
+  overflow-y: auto;
 
   &__submit-button {
     width: 100%;
